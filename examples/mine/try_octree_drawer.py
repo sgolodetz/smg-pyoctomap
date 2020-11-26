1e4b31ffb445c04f5cb54fc17753e46563809e6c
Stuart Golodetz <sgolodetz@gxstudios.net>
2020-11-26 22:21:17 +0000

    Make a nicer example.

--- examples/mine/try_octree_drawer.py
+++ examples/mine/try_octree_drawer.py
@@ -13,15 +13,28 @@ from typing import Tuple
 from pyoctomap import *
 
 
-def draw_frame(drawer: OcTreeDrawer) -> None:
+def draw_frame(drawer: OcTreeDrawer, height: float) -> None:
     glClearColor(1.0, 1.0, 1.0, 1.0)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
-    glTranslatef(0.0, 0.0, -50.0)
+    gluLookAt(0.0, -height * 2, height / 2, 0.0, 0.0, height / 2, 0.0, 0.0, 1.0)
+
+    glEnable(GL_BLEND)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+
+    glEnable(GL_LIGHTING)
+    glEnable(GL_LIGHT0)
+    pos: np.ndarray = np.array([-1.0, 1.0, 1.0, 0.0])
+    glLightfv(GL_LIGHT0, GL_POSITION, pos)
+
+    glEnable(GL_COLOR_MATERIAL)
 
     drawer.draw()
+
+    glDisable(GL_LIGHTING)
+    glDisable(GL_BLEND)
 
     pygame.display.flip()
 
@@ -32,7 +45,7 @@ def main() -> None:
     pygame.display.set_mode(window_size, pygame.DOUBLEBUF | pygame.OPENGL)
 
     glMatrixMode(GL_PROJECTION)
-    gluPerspective(45, (window_size[0] / window_size[1]), 0.1, 100.0)
+    gluPerspective(45, (window_size[0] / window_size[1]), 0.1, 1000.0)
 
     voxel_size: float = 1.0
     half_voxel_size: float = voxel_size / 2.0
@@ -40,13 +53,14 @@ def main() -> None:
     tree: OcTree = OcTree(voxel_size)
 
     origin: Vector3 = Vector3(half_voxel_size, half_voxel_size, half_voxel_size)
-    offset: Vector3 = Vector3(voxel_size * 10, 0.0, 0.0)
+    offset: Vector3 = Vector3(voxel_size * 20, 0.0, 0.0)
 
     drawer: OcTreeDrawer = OcTreeDrawer()
     drawer.enable_freespace()
 
     origin_pose: Pose6D = Pose6D(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-    angles: np.ndarray = np.linspace(0.0, 2 * math.pi, 256, endpoint=False)
+    angles: np.ndarray = np.linspace(0.0, 10 * math.pi, 1024, endpoint=False)
+    height: float = 100.0
     i: int = 0
 
     while True:
@@ -56,14 +70,21 @@ def main() -> None:
                 sys.exit(0)
 
         if i < len(angles):
-            angled_offset: Vector3 = offset.copy()
-            angled_offset.rotate_ip(0, 0, angles[i])
-            tree.insert_ray(origin, origin + angled_offset * (1 + i / (len(angles) - 1)))
+            horizontal_offset: Vector3 = offset.copy()
+            horizontal_offset.rotate_ip(0, 0, angles[i])
+            vertical_offset: Vector3 = Vector3(0, 0, i * height / len(angles))
+            tree.insert_ray(origin, origin + horizontal_offset + vertical_offset)
+            tree.insert_ray(origin, origin + horizontal_offset * 0.5 + vertical_offset)
             drawer.set_octree(tree, origin_pose)
             i += 1
+        else:
+            pygame.time.wait(2048)
+            break
 
-        draw_frame(drawer)
-        pygame.time.wait(2048 // len(angles))
+        draw_frame(drawer, height)
+        pygame.time.wait(1)
+
+    tree.write_binary("octree_drawer.bt")
 
 
 if __name__ == "__main__":
