4ee7c32b9283514bf768046cc9f59edf48270d26
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-05-18 17:22:38 +0100

    More refactoring.

--- smg/pyoctomap/__init__.py
+++ smg/pyoctomap/__init__.py
@@ -1,4 +1,3 @@
 from .cpp.pyoctomap import *
 from .python.octomap_picker import OctomapPicker
 from .python.octomap_util import OctomapUtil
-from .python.prespecified_octree_renderer import PrespecifiedOctreeRenderer
