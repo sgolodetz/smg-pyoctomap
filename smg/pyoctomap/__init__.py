0f4f639b43f8194192b1c062f20661b8676373eb
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-05-18 16:15:01 +0100

    Start work on PrespecifiedOctreeRenderer.

--- smg/pyoctomap/__init__.py
+++ smg/pyoctomap/__init__.py
@@ -1,3 +1,4 @@
 from .cpp.pyoctomap import *
 from .python.octomap_picker import OctomapPicker
 from .python.octomap_util import OctomapUtil
+from .python.prespecified_octree_renderer import PrespecifiedOctreeRenderer
