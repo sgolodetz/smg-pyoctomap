cadf76502c63f92f30322bdc8a174721eab3ac38
Stuart Golodetz <sgolodetz@gxstudios.net>
2022-05-03 13:24:57 +0100

    Introduce OpenGLDepthTestContext.

--- smg/pyoctomap/python/octomap_picker.py
+++ smg/pyoctomap/python/octomap_picker.py
@@ -3,7 +3,7 @@ import numpy as np
 from OpenGL.GL import *
 from typing import Tuple
 
-from smg.opengl import OpenGLFrameBuffer, OpenGLMatrixContext, OpenGLUtil
+from smg.opengl import OpenGLDepthTestContext, OpenGLFrameBuffer, OpenGLMatrixContext, OpenGLUtil
 from smg.rigging.helpers import CameraPoseConverter
 from smg.utility import GeometryUtil
 
@@ -42,40 +42,38 @@ class OctomapPicker:
                                     which pixels in the world-space points image are valid.
         """
         with self.__framebuffer:
-            # Set the viewport to encompass the whole framebuffer.
-            OpenGLUtil.set_viewport((0.0, 0.0), (1.0, 1.0), (self.__width, self.__height))
-
             # Enable depth testing.
-            glDepthFunc(GL_LEQUAL)
-            glEnable(GL_DEPTH_TEST)
+            with OpenGLDepthTestContext(GL_LEQUAL):
+                # Set the viewport to encompass the whole framebuffer.
+                OpenGLUtil.set_viewport((0.0, 0.0), (1.0, 1.0), (self.__width, self.__height))
 
-            # Clear the framebuffer.
-            glClearColor(0.0, 0.0, 0.0, 1.0)
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+                # Clear the framebuffer.
+                glClearColor(0.0, 0.0, 0.0, 1.0)
+                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-            # Set the projection matrix.
-            with OpenGLMatrixContext(GL_PROJECTION, lambda: OpenGLUtil.set_projection_matrix(
-                self.__intrinsics, self.__width, self.__height
-            )):
-                # Set the model-view matrix.
-                with OpenGLMatrixContext(GL_MODELVIEW, lambda: OpenGLUtil.load_matrix(
-                    CameraPoseConverter.pose_to_modelview(np.linalg.inv(world_from_camera))
+                # Set the projection matrix.
+                with OpenGLMatrixContext(GL_PROJECTION, lambda: OpenGLUtil.set_projection_matrix(
+                    self.__intrinsics, self.__width, self.__height
                 )):
-                    # Draw the octree.
-                    origin_pose: Pose6D = Pose6D(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-                    self.__drawer.set_octree(self.__tree, origin_pose)
-                    self.__drawer.draw()
+                    # Set the model-view matrix.
+                    with OpenGLMatrixContext(GL_MODELVIEW, lambda: OpenGLUtil.load_matrix(
+                        CameraPoseConverter.pose_to_modelview(np.linalg.inv(world_from_camera))
+                    )):
+                        # Draw the octree.
+                        origin_pose: Pose6D = Pose6D(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+                        self.__drawer.set_octree(self.__tree, origin_pose)
+                        self.__drawer.draw()
 
-                    # Get the contents of the depth buffer.
-                    depth_image: np.ndarray = OpenGLUtil.read_depth_image(self.__width, self.__height)
+                        # Get the contents of the depth buffer.
+                        depth_image: np.ndarray = OpenGLUtil.read_depth_image(self.__width, self.__height)
 
-                    # Compute the world-space points image.
-                    ws_points: np.ndarray = GeometryUtil.compute_world_points_image_fast(
-                        depth_image, world_from_camera, self.__intrinsics
-                    )
+                        # Compute the world-space points image.
+                        ws_points: np.ndarray = GeometryUtil.compute_world_points_image_fast(
+                            depth_image, world_from_camera, self.__intrinsics
+                        )
 
-                    # Compute the mask that indicates which world-space points are valid.
-                    mask: np.ndarray = np.where(depth_image <= 100.0, 255, 0).astype(np.uint8)
+                        # Compute the mask that indicates which world-space points are valid.
+                        mask: np.ndarray = np.where(depth_image <= 100.0, 255, 0).astype(np.uint8)
 
-                    # Return a tuple consisting of the masked world-space points image, and the mask itself.
-                    return np.where(np.atleast_3d(mask != 0), ws_points, np.zeros(3)), mask
+                        # Return a tuple consisting of the masked world-space points image, and the mask itself.
+                        return np.where(np.atleast_3d(mask != 0), ws_points, np.zeros(3)), mask
